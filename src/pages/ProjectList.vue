14496a7d2db62894d67224b26a71c2f249151a5d
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-06-12 21:59:00 +0200

    Feat: add pagination projectList page

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -9,30 +9,60 @@ export default {
     },
     data() {
         return {
-            projects: []
+            projects: [],
+            currentPage: 1,
+            nextPageUrl: null,
+            previousPageUrl: null
         };
     },
     methods: {
-        getProjects() {
-            axios.get('http://127.0.0.1:8000/api/projects')
+        getProjects(pageNumb) {
+            axios.get('http://127.0.0.1:8000/api/projects', {
+                params:{
+                    page: pageNumb
+                }
+            })
             .then((response) => {
-                this.projects = response.data.results;
+                this.projects = response.data.results.data;
+                this.currentPage = response.data.results.current_page;
+                this.previousPageUrl = response.data.results.prev_page_url;
+                this.nextPageUrl = response.data.results.next_page_url;
             });
         }
     },
     mounted() {
-        this.getProjects();
+        this.getProjects(this.currentPage);
     }
 }
 </script>
 
 <template>
-     <div class="container">
+
+    <div class="container">
         <h1>I nostri progetti</h1>
+
         <div class="row row-cols-3">
-            <ProjectCard v-for="project in projects" :projectInfo="project" :key="project.id"></ProjectCard>
+            <div v-for="project in projects" :key="project.id" class="col">
+                <ProjectCard :projectInfo="project"></ProjectCard>
+            </div>            
         </div>
+
+        <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <!-- <li class="page-item" v-if="previousPageUrl"> -->
+                    <li class="page-item ms-cursor" :class="{ disabled: previousPageUrl === 1}">
+                        <a class="page-link" @click="getProjects(currentPage-1)">Previous</a>
+                    </li>
+                    <li class="page-item" aria-current="page">
+                        <a class="page-link">{{ currentPage }}</a>
+                    </li>
+                    <li class="page-item ms-cursor" v-if="nextPageUrl">
+                        <a class="page-link" @click="getProjects(currentPage+1)">Next</a>
+                    </li>
+                </ul>
+        </nav>
     </div>
+
 </template>
 
 <style scoped lang="scss">
--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -27,23 +27,45 @@ export default {
 </script>
 
 <template>
-<div class="container">
-    
+<div class="container">    
     <div v-if="loading">
 
-        <h1>{{ project.name }}</h1>
+        <h2>{{ project.name }}</h2>
 
-        <div v-if="project.cover_image">
-            <img :src="`http://127.0.0.1:8000/storage/${project.cover_image}`" :alt="project.name">
-        </div>
-
-        <div class="row">
-            <h5>Cliente del progetto: {{ project.client_name }}</h5>
+        <div class="mt-4 d-flex justify-content-between">
+            <div>
+                <div v-if="project.client_name">
+                    <strong>Cliente del progetto:</strong>{{ project.client_name }}
+                </div>
+    
+                <div v-if="project.type">
+                    <strong>Tipo di progetto:</strong> {{ project.type.name }}
+                </div>
+    
+                <div v-if="project.technologies.lenght > 0">
+                    <strong>Tipo di progetto:</strong><span class="badge text-bg-info mx-1">{{ project.type.name }}</span>
+                </div>
+                
+                <div v-if="project.summary" class="row">                
+                    <h6>Descrizione del progetto</h6>
+                    <p class="card-text">{{ project.summary }}</p>
+                </div>
+            </div>         
             
-            <h6>Descrizione del progetto</h6>
-            <p class="card-text">{{ project.summary }}</p>
+            <div v-if="project.cover_image">
+                <img :src="`http://127.0.0.1:8000/storage/${project.cover_image}`" :alt="project.name">
+            </div>
+            
         </div>
+        
     </div>
-
 </div>
 </template>
+
+
+<style scoped lang="scss">
+
+
+
+
+</style>
